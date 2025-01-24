acbff6720dce3a7af8a68d7aa3d721b808c3e74c
Damian Sobek <sobe66@gmail.com>
2025-01-24 01:23:44 +0100

    Ready to deploy

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,25 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindAnimate from "tailwindcss-animate";
+
 export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {},
-		height: {
-        'screen-minus-nav': 'calc(100vh - 84px)',
-      },
-	  boxShadow: {
-        'bottom-only': '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
-      },
-	  screens: {
-        'mdCustom': '880px',
-      },
-  }
-},
-  plugins: [require("tailwindcss-animate")],
+    theme: {
+        extend: {
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            colors: {},
+            height: {
+                'screen-minus-nav': 'calc(100vh - 84px)',
+            },
+            boxShadow: {
+                'bottom-only': '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
+            },
+            screens: {
+                'mdCustom': '880px',
+            },
+        }
+    },
+    plugins: [tailwindAnimate],
 };
